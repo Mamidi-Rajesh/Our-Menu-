8fb3f0d92c8300c01e1bed1dac7427fc247a563f
MAMIDI RAJESH <101383614+Mamidi-Rajesh@users.noreply.github.com>
2022-11-15 15:26:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let list=document.querySelectorAll(".list li");
+let boxes= document.querySelectorAll(".box");
+
+list.forEach((el)=>{
+    el.addEventListener("click", (e)=>{
+        boxes.forEach((el2)=>{
+            el2.style.display = "none";
+        })
+        document.querySelectorAll(e.target.dataset.filter).forEach((li)=>{
+            li.style.display = "flex";
+        })
+    })
+})
